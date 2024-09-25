b6ec4b63e8e173765cd1d7e4c2c2eb0827709cf2
Shravan_124 <shravankumardharam124@gmail.com>
2024-09-25 17:32:14 +0530

    commita

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,14 +1,7 @@
-module.exports.index = function(req, res) {
-    res.render('index', { title: 'Eeshanth Reyhanth' });
+/* GET homepage */
+const index = (req, res) => {
+  res.render('index', { title: 'Express' });
   };
-  
-  module.exports.signin = function(req, res) {
-    res.render('signin', { title: 'Eeshanth Reyhanth' });
-  };
-  
-  module.exports.review = function(req, res) {
-    res.render('review', { title: 'Eeshanth Reyhanth' });
-  };
-  module.exports.register = function(req, res) {
-    res.render('register', { title: 'Eeshanth Reyhanth' });
-  };
+  module.exports = {
+      index
+      };
